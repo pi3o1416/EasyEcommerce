b83ec1418a6a78a695848237d4333775e32ffab6
Monir Hossain <mmonir.m.29@gmail.com>
2023-02-23 01:11:43 +0600

    Change Database from sqlite to postgresql.

--- ECommerce/settings.py
+++ ECommerce/settings.py
@@ -84,11 +84,14 @@ WSGI_APPLICATION = 'ECommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': CONFIG['DB_NAME'],
+        'USER': CONFIG['DB_USER'],
+        'PASSWORD': CONFIG['DB_PASS'],
+        'HOST': CONFIG['DB_HOST'],
+        'PORT': CONFIG['DB_PORT'],
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
