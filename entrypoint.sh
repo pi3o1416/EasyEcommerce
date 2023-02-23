6232f72041f2cf9181e906a0836f0aaac89a949a
Monir Hossain <mmonir.m.29@gmail.com>
2023-02-23 23:14:48 +0600

    :sparkles: Start new app banner.

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# Apply database migration
-echo "Apply Database Migration"
-python manage.py migrate --noinput
-
-# Start service
-echo "Starting service"
-python manage.py runserver 8000
