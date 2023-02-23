66484568dd162d1733f106d97aebec76f22c1cbc
Monir Hossain <mmonir.m.29@gmail.com>
2023-02-23 23:45:25 +0600

    Add Banner model of banner app.

--- banner/models.py
+++ banner/models.py
@@ -1,3 +1,26 @@
+
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-# Create your models here.
+
+class Banner(models.Model):
+    headline = models.CharField(
+        verbose_name=_("Banner Headline"),
+        max_length=500
+    )
+    image = models.ImageField(
+        verbose_name=("Banner Image")
+    )
+    top_text = models.CharField(
+        verbose_name=_("Top text"),
+        max_length=500,
+    )
+    bottom_text = models.CharField(
+        verbose_name=_("Bottom text"),
+        max_length=500,
+    )
+    optional_text = models.CharField(
+        verbose_name=_("Optional text"),
+        max_length=500,
+    )
+
