0f45f241eadfa45cdcbb28aab8ca07ad43f78752
Monir Hossain <mmonir.m.29@gmail.com>
2023-04-03 00:50:13 +0600

    Add Product related models to Admin Panel.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,34 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Product, ProductBrand, ProductCategory, ProductColor, ProductDiscount
+from .models import ProductTag
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Product._meta.fields]
+
+
+@admin.register(ProductBrand)
+class ProductBrandAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ProductBrand._meta.fields]
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ProductCategory._meta.fields]
+
+
+@admin.register(ProductColor)
+class ProductColorAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ProductColor._meta.fields]
+
+
+@admin.register(ProductDiscount)
+class ProductDiscountAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ProductDiscount._meta.fields]
+
+
+@admin.register(ProductTag)
+class ProductTagAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ProductTag._meta.fields]
