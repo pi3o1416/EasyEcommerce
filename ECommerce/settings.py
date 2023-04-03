20e5bad5dabaa692bda6fc1c5b15806ee19325ae
Monir Hossain <mmonir.m.29@gmail.com>
2023-04-03 23:46:45 +0600

    Add MEDIA settings to project settings.

--- ECommerce/settings.py
+++ ECommerce/settings.py
@@ -134,6 +134,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = 'static/'
+MEDIA_ROOT = 'media/'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
